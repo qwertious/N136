6a035c1b24155a67f9090ce9d61c2980e21981f0
qwertious <80864866+qwertious@users.noreply.github.com>
2022-06-25 22:56:40 -0400

    Add files via upload

--- N136.js
+++ N136.js
@@ -3,12 +3,12 @@ function go() {
     location.assign(number + ".html");
 }
 function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
+    var value = `; ${document.cookie}`;
+    var parts = value.split(`; ${name}=`);
     if (parts.length === 2) {
         return parts.pop().split(';').shift();
     } else return "";
 }
 if (getCookie("color") != "") {
-    document.getElementsById("body").style = "background-color: " + color;
+    document.getElementsById("body").style = "background-color: " + getCookie("color");
 }
